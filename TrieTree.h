a45f55f5b3313ac3bdf8d13c5165e75cf29262ba
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-11-02 11:01:55 +0200

    finish insert method

--- TrieTree.h
+++ TrieTree.h
@@ -7,10 +7,28 @@ using namespace std ;
 struct TrieNode{
     map<char, TrieNode*> children ;
     bool endOfWOrd ;
+
+    explicit TrieNode() : endOfWOrd(false) {}
 };
 
 class TrieTree {
+    TrieNode* root ;
 
+    TrieTree() = default ;
+
+    void insert(const string& key){
+        TrieNode* current = root ;
+
+        for (const char& c: key) {
+            TrieNode* node = current->children[c] ;
+            if (node == nullptr){
+                node = new TrieNode() ;
+                current->children.insert({c, node}) ;
+            }
+            current = node ;
+        }
+        current->endOfWOrd = true ;
+    }
 };
 
 # endif //TRIE_TREE_TRIETREE_H
